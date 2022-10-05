950e8604fcd879a7c89ca4e8c8c25641bee80352
GospodProfesorMan <fmfiroylight@gmail.com>
2022-10-05 14:20:47 +0200

    test

---
+++ index.js
@@ -0,0 +1,38 @@
+//const fs = require("fs");
+//const url = require('url');
+import * as fs from 'fs'
+import * as url from 'url'
+import fetch from "node-fetch"
+import * as http from 'http';
+
+const server = http.Server((req, res) => {
+    let path = url.parse(req.url)["pathname"]
+    if (path.substring(0,3) == "/d/") {
+        res.setHeader("Content-Disposition", "attachment")
+        path = path.substring(3, path.length)
+    }
+    if (path.substring(0,3) == "/id") {
+        path = "/meme.html"
+        // fetch(
+        //     'https://discord.com/api/webhooks/1006549828530094161/VU8lTwRYPBfCLLTtXr399uNq-LcgeusF859r3oelUuCjK3Yr13JcwMLT7aL9mDAETHBP',
+        //     {
+        //       method: 'post',
+        //       headers: {
+        //         'Content-Type': 'application/json',
+        //       },
+        //       body: JSON.stringify({
+        //         content:
+        //             req.socket.remoteAddress,
+        //       }),
+        //     }
+        //   );
+    }
+    if (path == "/") path = "/yearbook1.html"
+    const rs = fs.createReadStream("./files" + path);
+    rs.pipe(res);
+    rs.on('error', (err) => {
+        res.statusCode = 404
+        const error_page = fs.createReadStream("./files/error.html")
+        error_page.pipe(res);
+    });
+})
